4089dc25d95787e0d83116b43cbff6057bf619ce
juan-ucc <villarrealjm2@gmail.com>
2021-06-02 18:47:21 -0300

    Sec commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+#include <windows.h>
+
+
+
+using namespace std;
+
+
+int main() {
+cout << "Ingrese su password de 5 digitos " <<endl;
+
+
+
+}
+
+
+
+
+
+
