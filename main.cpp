f96cfbb85ca93c5529a90bbffa31c52858664234
juan-ucc <villarrealjm2@gmail.com>
2021-06-05 12:30:36 -0300

    fix

--- main.cpp
+++ main.cpp
@@ -12,36 +12,29 @@ int main(){
    for (int i=1;i<4;i++){
 
     cout << "Enter pass\n";
-   ch = _getch();
-   while(ch != 13){                     //character 13 es enter
-    if (ch != 8){                       //character 8 es enter
-        pass.push_back(ch);
-        cout << '*';
-
-    } else {
-        if (pass.length() > 0){
+    ch = _getch();
+    while(ch != 13){                        //character 13 es enter
+        if (ch != 8){                       //character 8 es enter
+            pass.push_back(ch);
+            cout << '*';
+        } else {
+            if (pass.length() > 0){
             cout << "\b \b";
             pass = pass.substr(0, pass.length() -1);
+            }
         }
-    }
-      ch = _getch();
+        ch = _getch();
    }
-   cout <<endl;
-
-
-
-   if(pass == "234"){
+    cout <<endl;
+    if(pass == "234"){
       cout << endl << "Bienvenido usuario"<<endl; break;
-   }else{
+    }else{
       cout << "Password incorrtecta, restan "<<3 - i<<" intentos"<<endl;
       if (i == 3) return 0;
-   }
+    }
 
-   }
+    }
 
-
+// Aca va el switch de lo que se quiera hacer
    cout <<" que desea hacer!" <<endl;
 }
-
-
-
