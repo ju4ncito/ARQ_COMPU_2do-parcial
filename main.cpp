bbb3d1cb46aaf80e1763a754b4d3f9fa5dac3716
juan-ucc <villarrealjm2@gmail.com>
2021-06-05 17:27:34 -0300

    Switch agregado

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <Windows.h>
-#include <conio.h> // getch
+#include <conio.h>
 #include <vector>
+#include <Windows.h>
+
 
 using namespace std;
+
+
+void retardo (unsigned long int);
+
+
 int main(){
+
+
    string pass ="";
    char ch;
    for (int i=1;i<4;i++){
 
-    cout << "Enter pass\n";
+    cout << "Ingrese su password de 5 digitos: "<<endl;
     ch = _getch();
     while(ch != 13){                        //character 13 es enter
         if (ch != 8){                       //character 8 es enter
@@ -27,14 +34,77 @@ int main(){
    }
     cout <<endl;
     if(pass == "234"){
-      cout << endl << "Bienvenido usuario"<<endl; break;
+      cout << endl << "Bienvenido al sistema! "<<endl; break;
     }else{
-      cout << "Password incorrtecta, restan "<<3 - i<<" intentos"<<endl;
-      if (i == 3) return 0;
-    }
+        if (i == 3) {
+                cout<<"Password no valida, no tiene mas intentos"<<endl;
+                return 0;
+        }
+        cout << "Password no valida, restan "<<3 - i<<" intentos "<<endl;
+        pass = "";
+
 
     }
 
-// Aca va el switch de lo que se quiera hacer
-   cout <<" que desea hacer!" <<endl;
+    }
+
+ //retardo para que se lea bienvenido
+  retardo(1000000000);
+
+
+
+  //Estructura switch
+
+        int choice = 0;
+
+   do {
+        system("cls");
+        cout << "Seleccione el numero de la secuencia a visualizar" << endl
+        << "(1) El choque" << endl
+        << "(2) El auto fantastico" << endl
+        << "(3) La carrera" << endl
+        << "(4) Salir " << endl;
+        while ((choice < 1)) {
+            cout << "Secuencia: ";
+            cin >> choice;
+        }
+        switch (choice) {
+            case 1:
+                system("cls");
+                cout<<"fantastic();";
+                choice = 0;
+                break;
+            case 2:
+                system("cls");
+                cout<<"crash()";
+                choice = 0;
+                break;
+            case 3:
+                system("cls");
+                cout<<"race()";
+                choice = 0;
+                break;
+            case 4:
+                system("cls");
+                cout<<"El sistema ha sido apagado"<<endl;
+                break;
+            default:
+                system("cls");
+                cout<<"El numero ingresado no corresponde a una secuencia valida"<<endl;
+                choice = 0;
+
+        }
+        cout << endl << endl << endl << "Presione ENTER para continuar ";
+        ch = getch();
+
+    } while (choice != 4);
+    return 0;
+
+}
+
+
+
+
+void retardo(unsigned long int a){
+    while (a) a--;
 }
