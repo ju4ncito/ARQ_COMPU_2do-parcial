64edd191a2720bb628fcaf16b7c4eb20a84d8d1d
juan-ucc <villarrealjm2@gmail.com>
2021-06-05 12:20:54 -0300

    Funcion del main menu

--- main.cpp
+++ main.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
+#include <string>
 #include <cstdlib>
-#include <conio.h>
-#include <windows.h>
-
-
+#include <Windows.h>
+#include <conio.h> // getch
+#include <vector>
 
 using namespace std;
+int main(){
+   string pass ="";
+   char ch;
+   for (int i=1;i<4;i++){
 
+    cout << "Enter pass\n";
+   ch = _getch();
+   while(ch != 13){ //character 13 is enter
+    if (ch != 8){
+        pass.push_back(ch);
+        cout << '*';
 
-int main() {
-
-    unsigned long int vel = 10000;
-    char password[5] = {'h','o','l','a','s'};
-    char caracter[6];
-    bool iguales = false;
-    int contador = 0;
-    cout << "Ingrese su password de 5 digitos " <<endl;
-
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),0);
-    cin.getline(caracter, 6);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),7);
-    iguales = true;
-
-    for (int i=0; i<5; i++){
-        cout<<"*";
-        if (password[i] != caracter[i]){
-            iguales = false;
+    } else {
+        if (pass.length() > 0){
+            cout << "\b \b";
+            pass = pass.substr(0, pass.length() -1);
         }
-        contador++;
-        _sleep(500);
-        system("CLS");
     }
+      ch = _getch();
+   }
+   cout <<endl;
 
+
+
+   if(pass == "234"){
+      cout << endl << "Bienvenido usuario"<<endl; break;
+   }else{
+      cout << "Password incorrtecta, restan "<<3 - i<<" intentos"<<endl;
+      if (i == 3) return 0;
+   }
+
+   }
 
 }
-
-
-
 
 
 
