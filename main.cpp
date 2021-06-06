0e73e24ef80329a053147effae195d3c9a79222c
juan-ucc <villarrealjm2@gmail.com>
2021-06-06 18:11:47 -0300

    Agregado choque los 5 y bateria

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ void mostrar (unsigned char);
 void choque();
 void carrera();
 void auto_fantastico();
+void choque_los_5();
+void bateria();
 
 
 int main(){
@@ -51,14 +53,9 @@ int main(){
     }
 
     }
-
- //retardo para que se lea bienvenido
+//retardo para que se lea bienvenido
   retardo(100000);
-
-
-
-  //Estructura switch
-
+//Estructura switch
         int choice = 0;
 
    do {
@@ -67,7 +64,9 @@ int main(){
         << "(1) El choque" << endl
         << "(2) El auto fantastico" << endl
         << "(3) La carrera" << endl
-        << "(4) Salir " << endl;
+        << "(4) Choque los 5!" << endl
+        << "(5) Bateria baja" << endl
+        << "(6) Salir " << endl;
         while ((choice < 1)) {
             cout << "Secuencia: ";
             cin >> choice;
@@ -90,6 +89,16 @@ int main(){
                 break;
             case 4:
                 system("cls");
+                choque_los_5();
+                choice = 0;
+                break;
+            case 5:
+                system("cls");
+                bateria();
+                choice = 0;
+                break;
+            case 6:
+                system("cls");
                 cout<<"El sistema ha sido apagado"<<endl;
                 break;
             default:
@@ -101,20 +110,14 @@ int main(){
         cout << endl << endl << endl << "Presione ENTER para continuar ";
         ch = getch();
 
-    } while (choice != 4);
+    } while (choice != 6);
     return 0;
 
 }
-
-
-
-
 void retardo(unsigned long int a){
     while (a) a--;
 }
-
 void mostrar (unsigned char dato){
-
   for (unsigned char mask = 128; mask > 0; mask>>=1)
     {
         if (dato&mask)
@@ -126,57 +129,87 @@ void mostrar (unsigned char dato){
         }
     }
 }
-
-
-
 void choque (){
   uint8_t tabla[] = {
     0x81, 0x42, 0x24, 0x18, 0x18, 0x24, 0x42
   };
-int j =8;
-do {
+while(1){
 
   for (int i = 0; i < 7; ++i) {
+    cout<< "Presione ENTER para volver al menu principal"<<endl;
     mostrar(tabla[i]);
     retardo(150000000);
     system("cls");
-  }
-} while (j==8);
+    if(GetKeyState(13) & 0x8000){return;}
+    }
+ }
 }
-
-
 void carrera (){
   uint8_t tabla[] = {
-    0x80, 0x80, 0x40, 0x40, 0x20, 0x20, 0x10, 0x10, 0x88, 0x48, 0x24, 0x14, 0xA, 0x6, 0x3,0x1
+    0x80, 0x80, 0x40, 0x40, 0x20, 0x20,
+    0x10, 0x10, 0x88, 0x48, 0x24, 0x14,
+    0xA, 0x6, 0x3,0x1
   };
-
+while(1){
   for (int i = 0; i < 16; ++i) {
+    cout<< "Presione ENTER para volver al menu principal"<<endl;
     mostrar(tabla[i]);
     retardo(150000000);
     system("cls");
+
+
+    if(GetKeyState(13) & 0x8000){return;}
   }
 }
-
-
-
+}
 void auto_fantastico(){
-  unsigned int dato = 0x80; // 1000 0000
+  unsigned int dato = 0x80;
+  while(1){
 
-  // right shift
   for (int i = 0; i < 8; ++i) {
+    cout<< "Presione ENTER para volver al menu principal"<<endl;
     mostrar(dato);
     dato >>= 1;
     retardo(100000000);
+
     system("cls");
+
+    if(GetKeyState(13) & 0x8000){return;}
   }
-
-
   dato = 0x02;
   for (int i = 0; i < 6; ++i) {
+    cout<< "Presione ENTER para volver al menu principal"<<endl;
     mostrar(dato);
     dato <<= 1;
     retardo(100000000);
     system("cls");
+    if(GetKeyState(13) & 0x8000){return;}
   }
-
+}
+}
+void choque_los_5(){
+ unsigned char tabla []={0x0,0x81, 0xC3, 0xE7, 0xFF, 0xE7, 0xC3, 0x81};
+    while (1) {
+        for (int i = 0; i < 8; i++) {
+            cout<< "Presione ENTER para volver al menu principal"<<endl;
+            mostrar(tabla[i]);
+            retardo(100000000);
+            system("cls");
+            if(GetKeyState(13) & 0x8000){return;}
+    }
+}
+}
+void bateria(){
+    unsigned int dato = 0xFF;
+  while(1){
+  for (int i = 0; i < 9; ++i) {
+    cout<< "Presione ENTER para volver al menu principal"<<endl;
+    mostrar(dato);
+    dato <<= 1;
+    retardo(100000000);
+    system("cls");
+    if(GetKeyState(13) & 0x8000){return;}
+  }
+dato = 0xFF;
+}
 }
